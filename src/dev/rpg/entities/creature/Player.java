3bfa82f865d72619e6875a3cd55b542901a4a267
Ryan <dragonguard7@gmail.com>
2016-05-24 16:53:18 -0400

    Made entity size dynamic. Updated classes for better organization and proper functions

--- src/dev/rpg/entities/creature/Player.java
+++ src/dev/rpg/entities/creature/Player.java
@@ -10,28 +10,36 @@ public class Player extends Creature{
 	private Game game;
 	
 	public Player(Game game, float x, float y) {
-		super(x, y);
+		super(x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
 		this.game = game;
 	}
 
 	public void tick() {
+		getInput();
+		move();
+	}
+	
+	private void getInput(){
+		xMove = 0;
+		yMove = 0;
+		
 		if(game.getKeyManager().up || game.getKeyManager().up2){
-			y -= 3;
+			yMove = -speed;
 		}
 		if(game.getKeyManager().down || game.getKeyManager().down2){
-			y += 3;
+			yMove = speed;
 		}
 		if(game.getKeyManager().left || game.getKeyManager().left2){
-			x -= 3;
+			xMove = -speed;
 		}
 		if(game.getKeyManager().right || game.getKeyManager().right2){
-			x += 3;
+			xMove = speed;
 		}
 		
 	}
 
 	public void render(Graphics g) {
-		g.drawImage(Assets.playerStill, (int)x, (int)y, null);
+		g.drawImage(Assets.playerStill, (int)x, (int)y, width, height,null); //(Image, Xpos, Ypos, Xsize, Ysize, observer)
 	}
 
 }
